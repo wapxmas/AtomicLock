b546f02ed9926dfc04ddee8329ee91b194c873b3
wapxmas <wapxmas@gmail.com>
2018-06-16 11:18:28 +0300

    Initial commit

---
+++ ALock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtomicLock
+{
+    public class ALock
+    {
+        private volatile object _lock = null;
+
+        public IDisposable Lock()
+        {
+            var dummyObject = new object();
+
+            var spinWait = new SpinWait();
+
+            while (Interlocked.CompareExchange(ref _lock, dummyObject, null) != dummyObject)
+            {
+                spinWait.SpinOnce();
+            }
+
+            return new ALockAcquired(this);
+        }
+
+        protected void Free()
+        {
+            Interlocked.Exchange(ref _lock, null);
+        }
+
+        private class ALockAcquired : IDisposable
+        {
+            private readonly ALock _lockedObject;
+
+            public ALockAcquired(ALock lockedObject)
+            {
+                _lockedObject = lockedObject;
+            }
+
+            public void Dispose()
+            {
+                _lockedObject.Free();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AtomicLock
+{
+    class Program
+    {
+        private static ALock _lock = new ALock();
+
+        private static volatile int counter = 0;
+
+        static void Main(string[] args)
+        {
+            var threads = Enumerable.Range(1, 10).Select(_ => CreateThread()).ToList();
+
+            threads.ForEach(t => t.Join());
+
+            Console.WriteLine($"expected: {threads.Count * 100000}, actual: {counter}");
+        }
+
+        private static Thread CreateThread()
+        {
+            var thread = new Thread(ThreadMain);
+            thread.IsBackground = true;
+            thread.Start();
+            return thread;
+        }
+
+        private static void ThreadMain()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                using (_lock.Lock())
+                {
+                    counter++;
+                }
+            }
+        }
+    }
+}
